1666b9c1889d1463f1e32bbd9b2b5dbcc553efad
raflisalam <raflisalam.dokumen@gmail.com>
2023-12-23 15:34:08 +0800

    feat: create dummy data for list menu and fetch the data into adapter

--- app/src/main/java/chaps/dev/customnavigationdrawer/MainActivity.kt
+++ app/src/main/java/chaps/dev/customnavigationdrawer/MainActivity.kt
@@ -2,10 +2,41 @@ package chaps.dev.customnavigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import chaps.dev.customnavigationdrawer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MenuNavigationAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar,
+            R.string.nav_drawer_open, R.string.nav_drawer_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        recyclerView = binding.navView.getHeaderView(0).findViewById(R.id.rvMenu)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+        adapter = MenuNavigationAdapter(MenuDataProvider.menuList, this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onItemMenuClick(slug: String) {
+        Toast.makeText(this, "Ini Toast Menu : $slug", Toast.LENGTH_SHORT).show()
+        binding.drawerLayout.closeDrawers()
     }
 }
---
+++ app/src/main/java/chaps/dev/customnavigationdrawer/MenuDataProvider.kt
@@ -0,0 +1,16 @@
+package chaps.dev.customnavigationdrawer
+
+object MenuDataProvider {
+    val menuList = listOf(
+        Menu("1", "Dashboard", "Dashboard", "&#xf0e4;"),
+        Menu("2", "List Setoran", "List Setoran", "&#xf019;"),
+        Menu("3", "Setoran Nasabah", "Setoran Nasabah", "&#xf067;"),
+        Menu("4", "List Sampah", "List Sampah", "&#xf1f8;"),
+        Menu("5", "List BS Unit", "List BS Unit", "&#xf1e0;"),
+        Menu("6", "List Nasabah", "List Nasabah", "&#xf0c0;"),
+        Menu("7", "Laporan", "Laporan", "&#xf0f6;"),
+        Menu("8", "Settings", "Settings", "&#xf082;"),
+        Menu("9", "Pengaturan Menu", "Pengaturan Menu", "&#xf0c9;"),
+        Menu("10", "Pengaturan Pengguna", "Pengaturan Pengguna", "&#xf007;")
+    )
+}
